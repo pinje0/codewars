ca17018113c816fa43b49d591a36ec69fa0c7a62
pinje0 <melvinaustinn@gmail.com>
2024-05-17 14:22:38 +0700

    17-05-2024

---
+++ javascript/8-kyu/jennyGreet.js
@@ -0,0 +1,6 @@
+function greet(name) {
+    return name == "Johnny" ? "Hello, my love!" : `Hello, ${name}!`
+}
+
+console.log(greet("Jim")) // "Hello, Jim!"
+console.log("Johnny") // Hello, my love!
---
+++ typescript/8-kyu/jennyGreet.ts
@@ -0,0 +1,3 @@
+export function greet(name: string): string {
+    return name === "Johnny" ? "Hello, my love!" : `Hello, ${name}!`
+}
