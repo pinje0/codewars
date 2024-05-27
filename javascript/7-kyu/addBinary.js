bc2ab4a7458443b2c4d811dae090e289a00f29bf
pinje0 <melvinaustinn@gmail.com>
2024-05-27 21:49:13 +0700

    27-05-2024

--- javascript/7-kyu/addBinary.js
+++
@@ -1,5 +0,0 @@
-function addBinary(a, b) {
-    return (a + b).toString(2)
-}
-
-console.log(addBinary(1, 1)) // "10" (1 + 1 = 2 in decimal or 10 in binary)
--- javascript/7-kyu/binaryArrayToNumber.js
+++
@@ -1,6 +0,0 @@
-const binaryArrayToNumber = (arr) => {
-    let j = arr.join("")
-    return parseInt(j, 2)
-}
-
-console.log(binaryArrayToNumber([0, 0, 1, 0])) // 0001 -> 1
