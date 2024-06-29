13aeae98f92f7669a6709b47f06c79e811215b49
pinje0 <melvinaustinn@gmail.com>
2024-06-29 20:55:02 +0700

    ２０２４年6月29日

---
+++ javascript/8-kyu/unusualFive.js
@@ -0,0 +1,8 @@
+function unusualFive() {
+    return "あいうえお".length
+}
+
+console.log(unusualFive()) // 5
+
+//TODO
+// should not use any banned characters ("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "+", "-", "*", "/")
